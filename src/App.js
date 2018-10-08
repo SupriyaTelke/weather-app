1ecb51400143df199cfe389d31202be0ff54f154
SupriyaTelke <supriyatelke@gmail.com>
2018-10-08 11:19:49 +0200

    API call and state initialization

--- src/App.js
+++ src/App.js
@@ -5,11 +5,39 @@ import {Form} from './components/form';
 import {Weather} from './components/weather';
 
 class App extends Component {
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  }
+
+  getWeather = async (e) => {
+    e.preventDefault();
+    const Api_Key="5959396a0bd213f7c25fb4f079ecbbed";
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${Api_Key}`);
+    const response = await api_call.json();
+    console.log('current weather',response);
+
+    this.setState({
+      temperature: response.main.temp,
+      city: response.name,
+      country: response.sys.country,
+      humidity: response.main.humidity,
+      description: response.weather[0].description,
+      error: ""
+    })
+  }
+
   render() {
     return (
       <div>
         <Title />
-        <Form />
+        <Form loadWeather={this.getWeather}/>
         <Weather />
       </div>
     );
--- src/components/form.js
+++ src/components/form.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
-export const Form = (props) => {
-    return (
-        <form>
-        <input type="text" name="city" placeholder="City..." />
-        <input type="text" name="country" placeholder="Country..." />
-        <button>Get Weather</button>
-      </form>
-    );
+export class Form extends React.Component {
+    render(){
+        return (
+            <form onSubmit={this.props.loadWeather}>
+            <input type="text" name="city" placeholder="City..." />
+            <input type="text" name="country" placeholder="Country..." />
+            <button>Get Weather</button>
+        </form>
+        );
+    }
 }
 
 export default Form;
